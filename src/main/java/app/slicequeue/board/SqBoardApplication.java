a710670aa4c6e8256d98983986e0823f60ee1f7c
slicequeue <slicequeue@gmail.com>
2025-04-06 10:22:13 +0900

    setting: 프로젝트 구축 - jpa, lombok, mysql

---
+++ src/main/java/app/slicequeue/board/SqBoardApplication.java
@@ -0,0 +1,13 @@
+package app.slicequeue.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/slicequeue/board/SqBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package app.slicequeue.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
