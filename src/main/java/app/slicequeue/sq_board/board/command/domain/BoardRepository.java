6414665dcf85c308b5c5f496d866ac2d3f73d78c
slicequeue <slicequeue@gmail.com>
2025-04-13 10:55:48 +0900

    feat: 게시판 글 수정 서비스 구현, 조회 리포지토리 선언 및 구현

--- src/main/java/app/slicequeue/sq_board/board/command/domain/BoardRepository.java
+++ src/main/java/app/slicequeue/sq_board/board/command/domain/BoardRepository.java
@@ -2,9 +2,13 @@ package app.slicequeue.sq_board.board.command.domain;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BoardRepository {
 
     Board save(Board board);
+
+    Optional<Board> findByBoardId(BoardId id);
 
 }
---
+++ src/test/java/app/slicequeue/sq_board/board/command/application/UpdateBoardServiceTest.java
@@ -0,0 +1,72 @@
+package app.slicequeue.sq_board.board.command.application;
+
+import app.slicequeue.common.exception.BadRequestException;
+import app.slicequeue.common.exception.NotFoundException;
+import app.slicequeue.sq_board.board.command.domain.Board;
+import app.slicequeue.sq_board.board.command.domain.BoardRepository;
+import app.slicequeue.sq_board.board.command.domain.dto.UpdateBoardCommand;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateBoardServiceTest {
+
+    @InjectMocks
+    UpdateBoardService updateBoardService;
+    @Mock
+    BoardRepository boardRepository;
+
+    @Captor
+    ArgumentCaptor<Board> boardArgumentCaptor;
+
+    @Test
+    void 유효한_수정커멘드로_게시판_수정시_정상적으로_저장된다() {
+        // given`
+        Board board = BoardFixture.createBoardExample1();
+        UpdateBoardCommand command = new UpdateBoardCommand(board.getBoardId(), "게시글1제목수정", 100L, "게시글1설명내용수정");
+
+        given(boardRepository.findByBoardId(board.getBoardId())).willReturn(Optional.of(board));
+        given(boardRepository.save(any())).willReturn(Mockito.mock(Board.class));
+
+        // when
+        updateBoardService.updateBoard(command);
+
+        // then
+        verify(boardRepository, times(1)).save(boardArgumentCaptor.capture());
+    }
+
+    @Test
+    void 메서드_호출시_인자로_잘못된_값_예외가_발생시_잘못된_예외로_되던진다() {
+        // given
+        Board board = BoardFixture.createBoardExample1();
+        UpdateBoardCommand command = new UpdateBoardCommand(board.getBoardId(), "게시글1제목수정", null, "게시글1설명내용수정");
+
+        given(boardRepository.findByBoardId(board.getBoardId())).willReturn(Optional.of(board));
+
+        // when & then
+        assertThatThrownBy(() ->  updateBoardService.updateBoard(command)).isInstanceOf(BadRequestException.class);
+    }
+
+    @Test
+    void 메서드_호출시_인자로_잘못된_값_예외가_아닌_다른예외_발생시_그대로_되던진다() {
+        // given
+        Board board = BoardFixture.createBoardExample1();
+        UpdateBoardCommand command = new UpdateBoardCommand(board.getBoardId(), "게시글1제목수정", null, "게시글1설명내용수정");
+
+        given(boardRepository.findByBoardId(board.getBoardId())).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() ->  updateBoardService.updateBoard(command)).isInstanceOf(NotFoundException.class);
+    }
+
+}
