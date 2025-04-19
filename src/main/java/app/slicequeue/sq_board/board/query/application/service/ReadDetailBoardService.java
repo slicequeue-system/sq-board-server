3aa8efd15a33414f474d06fb22501f6f1b90bb53
slicequeue <slicequeue@gmail.com>
2025-04-19 18:23:56 +0900

    feat: 서비스 작업 - 게시판 상세 조회 메서드 구현

---
+++ src/main/java/app/slicequeue/sq_board/board/query/application/service/ReadDetailBoardService.java
@@ -0,0 +1,31 @@
+package app.slicequeue.sq_board.board.query.application.service;
+
+import app.slicequeue.common.exception.NotFoundException;
+import app.slicequeue.sq_board.board.query.application.dto.ReadDetailQuery;
+import app.slicequeue.sq_board.board.query.infra.JpaBoardPagingQueryRepository;
+import app.slicequeue.sq_board.board.query.presentation.dto.BoardDetail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReadDetailBoardService {
+
+    private final JpaBoardPagingQueryRepository boardPagingQueryRepository;
+
+    public BoardDetail readDetail(ReadDetailQuery query) {
+        validateReadDetailParam(query);
+        return boardPagingQueryRepository.findBoardDetailBy(query.projectId(), query.boardId().getId())
+                .orElseThrow(() -> new NotFoundException(String.format("board not found. %s", query)));
+    }
+
+    private static void validateReadDetailParam(ReadDetailQuery query) {
+        Assert.notNull(query, "query must not be null.");
+        Assert.notNull(query.projectId(), "projectId must not be null.");
+        Assert.notNull(query.boardId(), "boardId must not be null.");
+    }
+
+}
