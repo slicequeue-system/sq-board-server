998458e5e758e416a102f85fe87e58aa3c72d8d4
slicequeue <slicequeue@gmail.com>
2025-04-29 00:07:28 +0900

    feat: 댓글 삭제 API

--- src/main/java/app/slicequeue/sq_board/comment/command/domain/CommentId.java
+++ src/main/java/app/slicequeue/sq_board/comment/command/domain/CommentId.java
@@ -4,6 +4,7 @@ import app.slicequeue.common.snowflake.Snowflake;
 import app.slicequeue.sq_board.common.base.AbstractSnowflakeId;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
@@ -20,7 +21,7 @@ public class CommentId extends AbstractSnowflakeId<CommentId> {
     }
 
     public static CommentId generateId() {
-        return new CommentId(snowflake.nextId());
+        return new CommentId().generate();
     }
 
     public static CommentId from(Long idValue) {
