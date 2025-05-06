0a2abfbbbfcbd6f388cacb1bfcb82c99d67b1cca
slicequeue <slicequeue@gmail.com>
2025-05-06 17:36:43 +0900

    fix: 댓글 이모지 카운트 집계 누락된 Lock 추가

--- src/main/java/app/slicequeue/sq_board/comment_reaction/command/domain/CommentReactionCountRepository.java
+++ src/main/java/app/slicequeue/sq_board/comment_reaction/command/domain/CommentReactionCountRepository.java
@@ -1,5 +1,7 @@
 package app.slicequeue.sq_board.comment_reaction.command.domain;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,8 @@ public interface CommentReactionCountRepository {
     
     CommentReactionCount save(CommentReactionCount commentReactionCount);
 
+    void delete(CommentReactionCount reactionCount);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<CommentReactionCount> findLockedByCommentReactionCountId(CommentReactionCountId commentReactionCountId);
 }
